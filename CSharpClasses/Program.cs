08df086a92a78e4c7adf0597eb034a77d8a5d52d
Joey Petrone <joey.petrone@gmail.com>
2020-08-03 21:16:03 -0500

    Add project files.

---
+++ CSharpClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
