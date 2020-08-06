a200e1c899bbc7e810d25600e18fe66185d006c2
Joey Petrone <joey.petrone@gmail.com>
2020-08-05 22:46:59 -0500

    Favorite Things class created to control specific app

--- CSharpClasses/Program.cs
+++ CSharpClasses/Program.cs
@@ -7,76 +7,34 @@ namespace CSharpClasses
     {
         static void Main(string[] args)
         {
-                            // Favorite Things //
-            //Puppy class
-            var puppy = new Puppy()
+            bool allDone = false;
+            do
             {
-                Name = "DeeDee",
-                Type = "Shih Tzu",
-                Age = 4,
-                Gender = Gender.Female
-            };
+                string userResponse = "";
 
-            puppy.Play(Activity.Ball);
-            puppy.Play(Activity.Stick);
-            puppy.Play(Activity.ChewToy);
-            puppy.Play(Activity.NoActivitySelected);
+                // Favorite Things //
+                Console.WriteLine("Would you like to play around with your favorite things?");
+                userResponse = Console.ReadLine();
+                if (userResponse.ToLower() != "n" && userResponse.ToLower() != "no")
+                {
+                    var playFavThings = new FavoriteThings.FavoriteThings();
 
-            puppy.Train(Trick.Sit);
-            puppy.Train(Trick.RollOver);
-            puppy.Train(Trick.PlayDead);
-            puppy.Train(Trick.NoTrickSelected);
+                    playFavThings.Run();
+                }
 
-            //JunkFood class
-            var pizza = new JunkFood(FoodType.Pizza)
-            {
-                Name = "Deep Dish"
-            };
+                Console.WriteLine("Are you done with the C# Classes program?");
+                userResponse = Console.ReadLine();
+                if (userResponse.ToLower() != "y" && userResponse.ToLower() != "yes")
+                {
+                    allDone = false;
+                }
+                else
+                {
+                    allDone = true;
+                }
 
-            pizza.Eat();
-            pizza.Eat();
-            pizza.Eat();
-            pizza.Eat();
-            pizza.Eat();
-            pizza.Regret();
-            pizza.Eat();
-            pizza.Eat();
-            pizza.Eat();
-
-
-            var iceCream = new JunkFood(FoodType.IceCream)
-            {
-                Name = "Tonight Dough"
-            };
-
-            iceCream.Eat();
-
-            var candy = new JunkFood(FoodType.Candy)
-            {
-                Name = "Reese's Peanut Butter Cup"
-            };
-
-            candy.Eat();
-
-            var nachos = new JunkFood(FoodType.Nachos)
-            {
-                Name = "Doritos"
-            };
-
-            nachos.Eat();
-
-            //VideoGame class
-            var pcGame = new VideoGame(GamingSystem.PC, "Solitare");
-
-            pcGame.LoadGame();
-
-            pcGame.Play();
-
-            var nintendoGame = new VideoGame(GamingSystem.Nintendo, "Zelda");
-
-            nintendoGame.LoadGame();
-
-            nintendoGame.Play();
+            } while (allDone == false);
+        
         }
     }
 }
